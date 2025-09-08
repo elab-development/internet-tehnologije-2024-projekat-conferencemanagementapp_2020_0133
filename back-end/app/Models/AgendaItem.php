5fc515e67ec3fc6a46a7aed1a90b705000c90ae4
Barxh <myxyq5@gmail.com>
2025-09-09 01:17:56 +0200

    feat: add AgendaItem model with migration and factory

---
+++ back-end/app/Models/AgendaItem.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AgendaItem extends Model
+{
+    /** @use HasFactory<\Database\Factories\AgendaItemFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'conference_id',
+        'title',
+        'description',
+        'speaker_id',
+        'type',
+        'start_time',
+        'end_time',
+    ];
+}
